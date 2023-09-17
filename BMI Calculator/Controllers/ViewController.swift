2b2540884377c06453f550cefe843094ab548836
aronbergman <brgmn@icloud.com>
2023-09-17 17:22:50 +0200

    feat(view): Arrêt 120. Learn How to Use a UISlider

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabelAction: UILabel!
+    @IBOutlet weak var weightLabelAction: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let formatValue = String(format: "%.2f", sender.value)
+        heightLabelAction.text = "\(formatValue) m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let formatValue = String(format: "%.0f", sender.value)
+        weightLabelAction.text = "\(formatValue) kg"
+    }
 }
 
